3044c0bad20191d86a1c7aad0408f0406744fed7
sarveshdakhore <72350223+sarveshdakhore@users.noreply.github.com>
2020-10-06 06:45:00 +0530

    Update IIEC_competetion.py

--- IIEC_competetion.py
+++ IIEC_competetion.py
@@ -1,8 +1,24 @@
 import os
 import platform
 import prat
-import imp
-import speech_recognition as sr
+try:
+    import imp
+except:
+    os.system("conda install imp")
+    import imp
+try:
+    import pyaudio
+except:
+    os.system("conda install pyaudio")
+
+
+try:
+    import speech_recognition as sr
+except ModuleNotFoundError:
+    os.system("pip install speechrecognition")
+    import speech_recognition as sr
+
+
 
 try:
     import time
